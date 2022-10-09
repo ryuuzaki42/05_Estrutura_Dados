5752f7738d7e67be6835ca22cecd52482f0933f0
João Batista <joao42lbatista@gmail.com>
2022-10-09 16:17:44 -0300

    gccCompileTestCode.sh - rm set

--- gccCompileTestCode.sh
+++ gccCompileTestCode.sh
@@ -4,9 +4,6 @@
 #
 # Last update: 09/10/2022
 #
-set -eEuo pipefail
-trap 'echo -e "\\n\\n\e[1;31mError at line $LINENO\033[0m - Command:\\n\e[1;31m$BASH_COMMAND\033[0m\\n"' ERR
-
 IFSOLD=$IFS
 
 IFS=$(echo -en "\\n\\b") # Change the Internal Field Separator (IFS) to "\\n\\b"
