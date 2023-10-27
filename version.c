4edfd269e4382182902153c2027b856099630941
João Batista <joao42lbatista@gmail.com>
2023-10-27 10:24:47 -0300

    Update version.c

--- version.c
+++ version.c
@@ -1,32 +1,42 @@
 #include<stdio.h>
+/*
+    Detecting the compiler version
+    gcc version.c // System default
 
-/*Detecting the compiler version
-    gcc version.c
+    gcc -std=c89 version.c
     gcc -std=c90 version.c
+
+    gcc -std=iso9899:199409 version.c
     gcc -std=c99 version.c
     gcc -std=c11 version.c
     gcc -std=c17 version.c
+    gcc -std=c2x version.c
+
+    -ansi
+        In C mode, this is equivalent to -std=c90
 */
 
 int main(){
     #ifndef __STDC_VERSION__
-        printf("Version: c89. __STDC_VERSION__ is not defined.\n");
+        printf("\nVersion: c89. __STDC_VERSION__ is not defined.\n\n");
     #else
-        #if (__STDC_VERSION__ == 199409L)
-            printf("Version: C94\n");
-        #endif
+        printf("\n__STDC_VERSION__ %ld - ", __STDC_VERSION__);
 
-        #if (__STDC_VERSION__ == 199901L)
-            printf("Version: C99\n");
-        #endif
-
-        #if (__STDC_VERSION__ == 201112L)
+        if (__STDC_VERSION__ == 199409L) {
+            printf("Version: C94\n\n");
+        } else if (__STDC_VERSION__ == 199901L) {
+            printf("Version: C99\n\n");
+        } else if (__STDC_VERSION__ == 201112L) {
             printf("Version: C11\n");
-        #endif
-
-        #if (__STDC_VERSION__ == 201710L)
-            printf("Version: C17\n");
-        #endif
+        } else if (__STDC_VERSION__ == 201710L) {
+            printf("Version: C17\n\n");
+        } else if (__STDC_VERSION__ > 201710L) {
+            printf("Version: C2x\n");
+            printf("The next version of the ISO C standard, still under development.\n");
+            printf("The support for this version is  experimental and incomplete.\n\n");
+        } else {
+            printf("Unknown standard\n\n");
+        }
     #endif
 
     return 0;
