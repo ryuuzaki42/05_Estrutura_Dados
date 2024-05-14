9df249e86a8f5ef3f5eaabdde2e02b07f88ef88d
João Batista <joao42lbatista@gmail.com>
2024-05-14 09:12:25 -0300

    Update dicas_C.r

--- dicas_C.r
+++ dicas_C.r
@@ -83,6 +83,53 @@
     scanf("%d %c %f", &n1, &c, &f);
     printf("n1: %d c: %c f: %f\n", n1, c, f);
 
+## Impressão tabular com espaço automáticos
+    /%-Xs => - alinhamento a esquerda, X o tamanho do texto, s o tipo
+    /%Xs => sem o -, alinhamento a direita
+
+    // %-12d %12d %-12f %12f
+
+    // %-12.3f %12.4f float e double com a quantidade de casas decimais
+
+    printf("\n%-12s %-12s %s\n", "Nome", "Idade", "Sexo");
+    printf("%-12s %-12d %c\n", "Marcelo", 39, 'M');
+    printf("%-12s %-12.3f %c\n", "Marina", 9.7, 'F');
+    printf("%-12s %-12d %c\n", "Luis", 11, 'M');
+
+    printf("\n%12s %12s %s\n", "Nome", "Idade", "Sexo");
+    printf("%12s %12d %c\n", "Marcelo", 39, 'M');
+    printf("%12s %12.4f %c\n", "Marina", 9.7, 'F');
+    printf("%12s %12d %c\n", "Luis", 11, 'M');
+
+## Valores grandes com muitos zeros
+    int teste = (1000 * 1000 * 1000);
+    printf("\n teste: %d", teste);
+
+    teste = 1e+9;
+    printf("\n teste: %d\n", teste);
+
+    #define _000 *1000
+    #define TAM 1 _000 _000
+
+    int main(){
+        printf("TAM: %d\n", TAM);
+    }
+
+## Versão do C - std version
+    ## Flags
+        -Wall      # warnings
+        -Wextra    # extra warning
+        -Wpedantic # enforces ISO C and ISO C++ standards
+        -O3        # -O Optimize, -O1 -O2 -O3, 3 optimize more, -O0 Reduce compilation time, is the default
+        -ansi      # In C, this is equivalent to -std=c90. In C++, it is equivalent to -std=c++98
+
+    ## -std
+        -std=$stdVersion
+        stdVersion="c99" # c89 c99 c11 c17 c18
+
+    gcc -Wall -Wextra -Wpedantic -O3 -std=$stdVersion "$file" -o test.out
+    gcc -Wall -Wextra -Wpedantic -O3 -ansi "$file" -o test.out
+
 ## Bons Links
     https://wiki.sj.ifsc.edu.br/index.php/SOP-stdio
     http://www.cs.ecu.edu/karl/2530/fall17/Notes/lec33A.html
