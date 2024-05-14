5c1eadaad4ad834d07f426c573150b02ef47b92b
João Batista <joao42lbatista@gmail.com>
2024-05-14 09:11:00 -0300

    Update dicas_C.r

--- dicas_C.r
+++ dicas_C.r
@@ -7,7 +7,7 @@
 # Mande me um e-mail. Ficarei Grato!
 # e-mail: joao42lbatista@gmail.com
 #
-# Last update: 02/04/2024
+# Last update: 14/05/2024
 #
 
 ## Leitura
@@ -55,10 +55,37 @@
 ## Em certas parte pode utilizar getchar para limpar partes do lixo do buffer do teclado
     mov = getchar();
 
+## Leitura de valores e descartar outros
+    int dia, mes, ano;
+    printf("\n1 Digite uma data no formato dia/mes/ano: ");
+    scanf("%d/%d/%d", &dia, &mes, &ano); // Descarta os /
+    printf("Data : %d/%d/%d", dia, mes, ano);
+
+    printf("\n2 Digite uma data no formato dia/mes/ano:: ");
+    scanf("%d%*c%d%*c%d", &dia, &mes, &ano); // Descarta qualquer character de tamanho 1
+    printf("Data: %d/%d/%d", dia, mes, ano);
+
+        // %*c would exclude one character. Remember, '/n' and '/t' are single characters
+        // %*d would exclude one integer
+        // %*f would exclude one float
+        // %*s would exclude one word
+
+## Dividir entrada em quantidade de dígitos
+    int n1, n2, n3;
+    printf("Digite um número com com pelo menos 6 digitos: ");
+    scanf("%1d%2d%d", &n1, &n2, &n3); // 1 dígito para n1, 2 em n2, os restar em n3
+    printf("n1: %d n2: %d n3: %d", n1, n2, n3);
+
+## Dividir entrada em tipos de valores
+    char c;
+    float f;
+    printf("\nDigite um numero, uma letra e um valor real: ");
+    scanf("%d %c %f", &n1, &c, &f);
+    printf("n1: %d c: %c f: %f\n", n1, c, f);
+
 ## Bons Links
     https://wiki.sj.ifsc.edu.br/index.php/SOP-stdio
     http://www.cs.ecu.edu/karl/2530/fall17/Notes/lec33A.html
     https://medium.com/@zoha131/fun-with-scanf-in-c-3d7a8d310229
     https://www.cprogramming.com/tips/tip/the-power-of-scanf
     https://codeforces.com/blog/entry/56932
-
